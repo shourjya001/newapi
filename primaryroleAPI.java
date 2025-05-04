e7ef928fcd91ccad1f3fa77485e6df2c23223d7f
shourjya001 <83701525+shourjya001@users.noreply.github.com>
2025-05-04 22:37:24 +0530

    Update primaryroleAPI.java

--- primaryroleAPI.java
+++ primaryroleAPI.java
@@ -99,5 +99,18 @@ public ResponseInternal sendPrimaryroleApi() throws IOException, JsonException {
         log.error("Unexpected status code: " + status);
     }
 
+// print to check
+    if (responseObject != null) {
+    try {
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonResponse = mapper.writeValueAsString(responseObject);
+        System.out.println("Full API Response as JSON: " + jsonResponse);
+        // Optionally log it as well
+        log.info("Full API Response as JSON: " + jsonResponse);
+    } catch (JsonProcessingException e) {
+        e.printStackTrace();
+    }
+}
+
     return responseObject;
 }
